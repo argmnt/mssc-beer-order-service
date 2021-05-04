9270b223498240342eab1400d98170dc0f3d21e3
argmnt <yagizgazibaba1996@hotmail.com>
2021-05-04 20:43:30 +0300

    validate order request message added with action.

---
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/ValidateOrderAction.java
@@ -0,0 +1,46 @@
+package guru.sfg.beer.order.service.sm.actions;
+
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManager;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
+import guru.sfg.beer.order.service.services.BeerOrderServiceImpl;
+import guru.sfg.beer.order.service.web.mappers.BeerOrderMapper;
+import guru.sfg.brewery.model.events.ValidateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final BeerOrderRepository beerOrderRepository;
+
+    private BeerOrderMapper beerOrderMapper;
+
+    private final JmsTemplate jmsTemplate;
+
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        Optional<BeerOrder> beerOrderOptional = beerOrderRepository.findById(UUID.fromString(beerOrderId));
+        BeerOrder beerOrder = beerOrderOptional.orElseThrow(() -> new RuntimeException());
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE, ValidateOrderRequest.builder()
+        .beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder))
+        .build());
+        log.debug("Sent Validation request to queue for order id " + beerOrderId);
+    }
+}
